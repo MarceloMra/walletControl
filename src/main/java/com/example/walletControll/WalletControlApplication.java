d37682935f292b42073fed6253d5e935a102d89e
MarceloMra <moreira.marcelo321@gmail.com>
2022-11-30 21:28:58 -0300

    Initial commit

---
+++ src/main/java/com/example/walletControll/WalletControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.walletControll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/walletControll/WalletControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletControll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
