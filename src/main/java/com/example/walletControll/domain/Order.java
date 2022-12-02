2ea1a7f4ffb9e4b8a3417285fa3bb76d2744d290
MarceloMra <moreira.marcelo321@gmail.com>
2022-12-02 00:20:53 -0300

    Refactoring Order domain class imports, its @Column annotations, and id generation strategy.

--- src/main/java/com/example/walletControll/domain/Order.java
+++ src/main/java/com/example/walletControll/domain/Order.java
@@ -2,15 +2,11 @@ package com.example.walletControll.domain;
 
 
 import com.example.walletControll.enums.OperationType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
@@ -18,8 +14,9 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false)
@@ -34,12 +31,9 @@ public class Order {
     @Column(nullable = false)
     private int quantity;
 
-    @Column
     private float priceOrAdjust;
 
-    @Column
     private float taxes;
 
-    @Column
     private String broker;
 }
