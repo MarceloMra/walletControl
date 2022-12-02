2034284e36abb188b75519f1d4b4f1885ff8f9ac
MarceloMra <moreira.marcelo321@gmail.com>
2022-12-02 00:18:15 -0300

    Downgrading spring boot version and adding h2 and openapi dependencies to the project.

--- src/main/java/com/example/walletControll/WalletControlApplication.java
+++ src/main/java/com/example/walletControll/WalletControlApplication.java
@@ -1,9 +1,11 @@
 package com.example.walletControll;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class WalletControlApplication {
 
 	public static void main(String[] args) {
